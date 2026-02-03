a2a0ef6d82f883663d094e8940eef9465506d4fe
Albar Malik <albar2806@student.abudzar.sch.id>
2026-02-03 10:35:09 +0700

    lupa gaada assetsnya

--- src/data/products.js
+++ src/data/products.js
@@ -1,48 +1,48 @@
 export const prList = [
     {
-        image: "/assets/images/bottle_green.png",
+        image: "/images/bottle_green.png",
         prName: "WBA Glance",
         price: "200.000",
         stock: false
     },
     {
-        image: "/assets/images/red_wba.png",
+        image: "/images/red_wba.png",
         prName: "WBA Gouache",
         price: "200.000",
         stock: true
     },
     {
-        image: "/assets/images/teal_wba.png",
+        image: "/images/teal_wba.png",
         prName: "WBA Flexi",
         price: "200.000",
         stock: true
     },
     {
-        image: "/assets/images/blue_wba.png",
+        image: "/images/blue_wba.png",
         prName: "WBA Droid",
         price: "200.000",
         stock: true
     },
     {
-        image: "/assets/images/yellow_wba.png",
+        image: "/images/yellow_wba.png",
         prName: "WBA Xander",
         price: "200.000",
         stock: true
     },
     {
-        image: "/assets/images/bottle_sakura.png",
+        image: "/images/bottle_sakura.png",
         prName: "WBA Youkoso",
         price: "210.000",
         stock: true
     },
     {
-        image: "/assets/images/bottle_prem1.png",
+        image: "/images/bottle_prem1.png",
         prName: "WBA Zenzai",
         price: "210.000",
         stock: true
     },
     {
-        image: "/assets/images/bottle_prem2.png",
+        image: "/images/bottle_prem2.png",
         prName: "WBA Intona",
         price: "220.000",
         stock: true
