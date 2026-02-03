34928814b7fbd9ad684b2d72e93b8e3e70254ac5
Albar Malik <albar2806@student.abudzar.sch.id>
2026-02-03 10:14:20 +0700

    gambar ke public

--- src/components/Sections/Hero/Hero.jsx
+++ src/components/Sections/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import "../Sections.css"
 
 export default function Home(){
     return(
-        <section id="home" className="w-full h-screen flex flex-col items-center justify-center text-center gap-5 bg-[url('/src/assets/images/abstract.png')] bg-no-repeat bg-cover bg-center">
+        <section id="home" className="w-full h-screen flex flex-col items-center justify-center text-center gap-5 bg-[url('/assets/images/abstract.png')] bg-no-repeat bg-cover bg-center">
             <h1 className="text-6xl font-bold text-trim mb-5">The World's No. 1<br/>Water Bottle Apprentice</h1>
             <h2 className="w-[30%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptatum, quidem ab nobis maiores saepe fugit inventore.</h2>
             <a href="#products" className="rounded-full px-5 py-4 text-trim border hover:bg-white hover:text-black transition-colors duration-200">Explore our Product</a>
